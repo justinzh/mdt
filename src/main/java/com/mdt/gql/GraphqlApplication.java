6de5b79044f1c2f9eef4cac63c9f874df94cb0b9
Justin Zhang <justin.z.zhang@gmail.com>
2021-07-08 02:08:53 -0500

    added cors setting

--- src/main/java/com/mdt/gql/GraphqlApplication.java
+++ src/main/java/com/mdt/gql/GraphqlApplication.java
@@ -2,6 +2,8 @@ package com.mdt.gql;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class GraphqlApplication {
@@ -9,4 +11,12 @@ public class GraphqlApplication {
 		SpringApplication.run(GraphqlApplication.class, args);
 	}
 
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/api/v1/pod").allowedOrigins("*");
+			}
+		};
+	}
 }
